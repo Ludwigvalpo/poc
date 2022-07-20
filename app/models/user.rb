8631a0b1b0c7050b318c2d08b2ff168687002c8f
Ludwig Valdenegro <ludwigval@gmail.com>
2022-07-19 21:04:45 -0400

    Agrega Home Index

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :photos
+  
   validates :first_name, :last_name, :rut, presence: true
   enum role: %i[user admin]
+
 end
